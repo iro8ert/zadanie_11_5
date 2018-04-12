c0533258145066b74967c17a98805300b7c956fd
Robert Nowaczyk <iro8ert@o2.pl>
2018-04-12 17:20:47 +0200

    add repo

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function Button() {
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		$('body').append(this.$element);
+	}
+}
+
+btn1.create();
